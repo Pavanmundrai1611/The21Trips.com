05160a58a1525b86e0a34c653db69c2e556712e9
Your Name <your@email.com>
2024-01-01 20:02:44 +0530

    backend done

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8080';
+const url = 'https://the21trips-server.onrender.com';
 
 export const addUserBookings = async (data) => {
   try {
